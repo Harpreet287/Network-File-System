4c78f100a0d5c2848752e54e026873c04c1b7861
Harpreet287 <124236553+Harpreet287@users.noreply.github.com>
2024-07-17 13:39:51 +0530

    resolving conflicts/clearing files

--- README.md
+++
@@ -1,5 +0,0 @@
-# Network-File-System
-A Distributed File System over TCP
-
-
-Run ss with g++ -std=c++17 ss.cpp client_ss.cpp nameserver_ss.cpp connections.cpp
--- connection.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONNECTION_H
-#define CONNECTION_H
-#include "imports.h"
-
-extern int createSocket(int port);
-extern void bindSocket(int sock, int port);
-extern void listenSocket(int sock);
-
-#endif 
